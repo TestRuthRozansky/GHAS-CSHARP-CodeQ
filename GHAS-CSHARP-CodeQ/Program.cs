516f7b7d2604858c0d6d6a2ea31564494a0a386c
TestRuthRozansky <109096095+TestRuthRozansky@users.noreply.github.com>
2022-07-29 16:11:55 -0400

    Add files via upload
    First commit

---
+++ GHAS-CSHARP-CodeQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GHAS_CSHARP_CodeQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d=5;
+            int n = 6;
+            int res = Test(d,n);
+
+            string userName = Console.ReadLine();
+            Regex pwd= new Regex(userName);
+
+        }
+
+        static int Test(int x, int y)
+        {
+            if (x >= y) return x / y;
+            if (x<y) return y / x; 
+
+            int z = x + y;
+
+            return z;
+        }
+    }
+}
